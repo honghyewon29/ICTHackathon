ef504c772edd90717be104dda0bfdf78da12fd0e
qkralsgh <qkralsgh0624@naver.com>
2025-05-13 23:10:02 +0900

    7번째 줄

--- LLM/Food_7d.py
+++ LLM/Food_7d.py
@@ -4,7 +4,7 @@ import schedule
 import time
 import os
 
-OUTPUT_FILE = "Data/식단표.txt"
+OUTPUT_FILE = "LLM\Data\식단표.txt"
 
 def fetch_meal_info(menuno, place_name, wanted_meals):
     url = f"https://www.suwon.ac.kr/index.html?menuno={menuno}"
